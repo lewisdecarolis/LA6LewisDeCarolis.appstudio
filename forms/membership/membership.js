115de0205ac172d22f2e3290bdef05a511d05833
lewisdecarolis <lsd81882@creighton.edu>
2021-03-15 19:32:27 -0500

    member?

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,10 @@
-/*
-let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+
+btnMember.onclick=function(){
+  if (members.includes(inptMember.value))
+  lblMember.value = "You are already a member"
+  else 
+   lblMember.value = members.push(inptMember.value) && "You are now a member"
+}
